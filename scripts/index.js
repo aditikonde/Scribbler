36c865e8de223ee61fd8242e736fae6709fccfcb
AditiKonde <aditi.konde.25@gmail.com>
2021-02-14 04:17:42 +0530

    Part-C complete

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,25 @@
 function openLoginForm() {
+    // var x = document.getElementsByTagName("BODY")[0];
+    // x.style.filter = "blur(3px) grayscale(90%)";
+
+    // var y = document.getElementById('login-popup');
+    // y.style.filter = "blur(0px) grayscale(0%)";
+
+
+
+
     document.body.classList.add("showLoginForm");
+
+    // var z = document.querySelectorAll("div:not('#login-popup .modal-popup popup')");
+    // z.style.filter = "blur(3px) grayscale(90%)";
 }
 
 function closeLoginForm() {
+    // var x = document.getElementsByTagName("BODY")[0];
+    // x.style.filter = "none";
+    // var mainContainer = document.getElementsByClassName('main-container')[0];
+    // mainContainer.classList.remove("showLoginForm");
+
     document.body.classList.remove("showLoginForm");
 }
 
